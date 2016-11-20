949913a3602bb78d6ee5c1ab7ffdd4a1e2ac7b63
gravitywp <gravitywp@gmail.com>
2016-11-20 14:43:30 +0800

    optimaze default UI setting

---
+++ core/ui.el
@@ -0,0 +1,29 @@
+;; UI optimzations and tweaks.
+
+;; This file is not part of GNU Emacs.
+
+;; the toolbar is just a waste of valuable screen estate
+;; in a tty tool-bar-mode does not properly auto-load,and is
+;; already disabled anyway.
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+
+(menu-bar-mode -1)
+
+;; the blinking cursor is nothing, but an annoyance.
+(blink-cursor-mode -1)
+
+;; disable startup screen
+(setq inhibit-startup-screen t)
+
+;; enable y/n answers
+(fset 'yes-or-no-p 'y-or-n-p)
+
+
+(setq frame-title-format
+      '("" invocation-name " - " (:eval (if (buffer-file-name)
+                                            (abbreviate-file-name (buffer-file-name))
+                                          "%b"))))
+;; disable the annoying bell ring
+(setq ring-bell-function 'ignore)
+
--- init.el
+++ init.el
@@ -10,13 +10,11 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
-
 ;; reduce the frequency of garbage collection by making it happen on
 ;; each 100MB of allocated data (the default is on every 0.76MB).
 (setq gc-cons-threshold 100000000)
 ;; display message at start and end of garbage colleciton.
 (setq garbage-collection-message t)
-
 
 (defvar package-list
   '(go-mode)
@@ -32,3 +30,9 @@
   )
 
 (install-package)
+
+;; 
+(defvar root-dir (file-name-directory load-file-name)
+  "The root dir of the Emacs configuration.")
+
+(load-file (concat root-dir "core/ui.el"))
