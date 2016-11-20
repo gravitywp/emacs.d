aeaf1376cb9f085ff1b020fca5caade7b8f98b5d
gravitywp <gravitywp@gmail.com>
2016-11-20 15:32:36 +0800

    add tsinghua elpa mirror && use doom-themes(atom-like)

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("e0d42a58c84161a0744ceab595370cbe290949968ab62273aed6212df0ea94b4" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(require 'doom-themes)
+(load-theme 'doom-one t)
+;;; OPTIONAL
+;; brighter source buffers
+(add-hook 'find-file-hook 'doom-buffer-mode)
+;; brighter minibuffer when active
+(add-hook 'minibuffer-setup-hook 'doom-brighten-minibuffer)
+;; Custom neotree theme
+(require 'doom-neotree)
--- init.el
+++ init.el
@@ -6,9 +6,10 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+             '("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/"))
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+             '("melpa-stable" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa-stable/
+") t)
 (package-initialize)
 ;; reduce the frequency of garbage collection by making it happen on
 ;; each 100MB of allocated data (the default is on every 0.76MB).
@@ -17,8 +18,11 @@
 (setq garbage-collection-message t)
 
 (defvar package-list
-  '(go-mode)
+  '(go-mode
+    doom-themes
+    )
   "A list of packages to ensure are installed at launch.")
+
 (defun install-package ()
   (when (not package-archive-contents)
     (package-refresh-contents))
@@ -35,4 +39,7 @@
 (defvar root-dir (file-name-directory load-file-name)
   "The root dir of the Emacs configuration.")
 
+(setq custom-file (concat root-dir "custom.el"))
+
 (load-file (concat root-dir "core/ui.el"))
+(load custom-file)
