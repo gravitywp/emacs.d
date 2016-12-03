e2ffe0139ff0b96270f04f85e4e4f7f5c49aa799
gravitywp <gravitywp@gmail.com>
2016-12-03 22:05:07 +0800

    update custom key-bindings

--- core/key-bindings.el
+++ core/key-bindings.el
@@ -5,6 +5,8 @@
 ;;Helm
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x f") 'helm-recentf)
+(global-set-key (kbd "C-x y") 'helm-show-kill-ring)
 
 
 
