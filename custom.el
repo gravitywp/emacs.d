c14d0da9d14b1e2c85171b277bbdf812a5f4f6b7
gravitywp <gravitywp@gmail.com>
2016-12-28 10:18:53 +0800

    [fix] wrong comment

--- custom.el
+++ custom.el
@@ -27,17 +27,12 @@
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init fil
-
-
- e should contain only one such instance.
+ ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
 
 (require 'doom-themes)
-(
-
- load-theme 'doom-one t)
+(load-theme 'doom-one t)
 ;;; OPTIONAL
 ;; brighter source buffers
 (add-hook 'find-file-hook 'doom-buffer-mode)
